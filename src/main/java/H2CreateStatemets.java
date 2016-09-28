dd361003388cfb3c6a80bf851414b2ae134eb32b
Faviana Lopez <favianalopez@Favianas-MacBook-Air.local>
2016-09-28 11:17:46 -0400

    user Blog

--- src/main/java/H2CreateStatemets.java
+++ src/main/java/H2CreateStatemets.java
@@ -3,36 +3,40 @@
  */
 public class H2CreateStatemets {
 
-   /* CREATE TABLE `user` (
-	`use_id` INT NOT NULL,
-	`use_name` varchar(255) NOT NULL,
-	`use_lastName` varchar(255) NOT NULL,
-	`use_email` varchar NOT NULL,
-	`use_password` varchar NOT NULL,
-	`use_login` varchar NOT NULL UNIQUE,
-	PRIMARY KEY (`use_id`)
+   /*
+   CREATE TABLE IF NOT EXIST user
+   (
+	use_id IDENTITY,
+	use_name VARCHAR(255),
+	use_lastName VARCHAR(255),
+	use_email VARCHAR,
+	use_password VARCHAR,
+	use_login VARCHAR,
+	PRIMARY KEY (use_id)
 );
 
-CREATE TABLE `blog` (
-	`blo_id` INT NOT NULL,
-	`blo_title` varchar NOT NULL,
-	`blo_date` varchar NOT NULL,
-	`blo_use_login` varchar NOT NULL,
-	PRIMARY KEY (`blo_id`)
+CREATE TABLE IF NOT EXIST blog
+(
+	blo_id INT,
+	blo_title VARCHAR,
+	blo_date VARCHAR,
+	blo_use_login VARCHAR,
+	PRIMARY KEY (blo_id)
 );
 
-CREATE TABLE `entry` (
-	`ent_id` INT NOT NULL,
-	`ent_title` varchar NOT NULL,
-	`ent_text` varchar NOT NULL,
-	`ent_date` varchar NOT NULL,
-	`ent_blo_id` INT NOT NULL,
-	PRIMARY KEY (`ent_id`)
+CREATE TABLE IF NOT EXIST entry
+(
+	ent_id INT,
+	ent_title VARCHAR,
+	ent_text VARCHAR,
+	ent_date VARCHAR,
+	ent_blo_id VARCHAR,
+	PRIMARY KEY (ent_id)
 );
 
-ALTER TABLE `blog` ADD CONSTRAINT `blog_fk0` FOREIGN KEY (`blo_use_login`) REFERENCES `user`(`use_id`);
+ALTER TABLE blog ADD CONSTRAINT blog_fk0 FOREIGN KEY (blo_use_login) REFERENCES user(use_id);
 
-ALTER TABLE `entry` ADD CONSTRAINT `entry_fk0` FOREIGN KEY (`ent_blo_id`) REFERENCES `blog`(`blo_id`);
+ALTER TABLE entry ADD CONSTRAINT entry_fk0 FOREIGN KEY (ent_blo_id) REFERENCES blog(blo_id);
 
     */
 
